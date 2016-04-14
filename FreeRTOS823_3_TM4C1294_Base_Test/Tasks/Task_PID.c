3db042fd0a3999b5ece8346689674d0a55c498ee
Chris Allmon <chrisa.freepress@gmail.com>
2016-04-14 17:12:38 -0500

    added other modules

---
+++ FreeRTOS823_3_TM4C1294_Base_Test/Tasks/Task_PID.c
@@ -0,0 +1,57 @@
+/*--Task_PID.c
+ *
+ *  Author:			Chris Allmon
+ *	Organization:	KU/EECS/EECS 690
+ *  Date:			April 14, 2016
+ *
+ *  Description: Transmits data via UART to PC.
+ *
+ */
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include "Drivers/uartstdio.h"
+#include "Tasks/Task_ReportData.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+//
+// Define the ReportData Queue
+//
+extern QueueHandle_t PID_Queue = NULL;
+
+extern uint32_t UART_Initialization();
+//
+// Define the ReportData Task
+//
+extern void Task_ReportData( void *pvParameters ) {
+	ReportData_Item theReport;
+	BaseType_t ReportQueue_Status;
+	//
+	// Insure UARTStdio is initialized
+	//
+	UART_Initialization();
+	//
+	// Define PID_Queue
+	//
+	PID_Queue = xQueueCreate( 10, sizeof( ReportData_Item ) );
+
+	while ( 1 ) {
+	//
+	// Try to read ReportItem from ReportData_Queue.
+	// If a ReportData_Item is returned, print the contents
+	//     to the UART via UARTStdioPrintf
+	//
+		ReportQueue_Status = xQueueReceive( PID_Queue,
+						&theReport,
+						 10 * portTICK_PERIOD_MS );
+		if ( ReportQueue_Status == pdTRUE ) {
+			UARTprintf( "%08d,%02d,%d,%d\n",
+					theReport.TimeStamp, theReport.ReportName,
+					theReport.ReportValue_0, theReport.ReportValue_1 );
+		}
+	}
+}
+
+
---
+++ FreeRTOS823_3_TM4C1294_Base_Test/Tasks/Task_TempCalc.h
@@ -0,0 +1,39 @@
+/*--Task_TempCalc.h
+ *
+ *  Author:			Chris Allmon
+ *	Organization:	KU/EECS/EECS 690
+ *  Date:			April 14, 2016
+ *
+ *  Description: Transmits data via UART to PC.
+ *
+ */
+#ifndef TASKS_TASK_REPORTDATA_H_
+#define TASKS_TASK_REPORTDATA_H_
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include "Drivers/uartstdio.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+//
+// Reference SysTickCount
+//
+extern volatile uint32_t xPortSysTickCount;
+//
+// Define the TempCalc Task
+//
+extern void Task_TempCalc( void *pvParameters );
+//
+// Define the Temp Queue
+//
+extern QueueHandle_t Temp_Queue;
+//
+// Define a structure to hold a data report
+//
+typedef struct ReportData_Item {uint32_t TimeStamp;
+uint32_t ReportName;
+uint32_t ReportValue_0;
+uint32_t ReportValue_1; } ReportData_Item;
+#endif /* TASKS_TASK_REPORTDATA_H_ */
